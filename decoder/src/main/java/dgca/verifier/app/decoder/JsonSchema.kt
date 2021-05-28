ff40d870e54d30e62631fc04592ce7ca4fe1c42e
oleksandrsarapulovgl <82441124+oleksandrsarapulovgl@users.noreply.github.com>
2021-05-28 13:21:43 +0300

    Schema update to support nullable values (#16)
    * Schema update to support nullable values
    
    * Updated schema to make not required fields nullable

--- decoder/src/main/java/dgca/verifier/app/decoder/JsonSchema.kt
+++ decoder/src/main/java/dgca/verifier/app/decoder/JsonSchema.kt
@@ -58,7 +58,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "    },\n" +
         "    \"v\": {\n" +
         "      \"description\": \"Vaccination Group\",\n" +
-        "      \"type\": \"array\",\n" +
+        "      \"type\": [\"null\", \"array\"],\n" +
         "      \"items\": {\n" +
         "        \"\$ref\": \"#/\$defs/vaccination_entry\"\n" +
         "      },\n" +
@@ -66,7 +66,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "    },\n" +
         "    \"t\": {\n" +
         "      \"description\": \"Test Group\",\n" +
-        "      \"type\": \"array\",\n" +
+        "      \"type\": [\"null\", \"array\"],\n" +
         "      \"items\": {\n" +
         "        \"\$ref\": \"#/\$defs/test_entry\"\n" +
         "      },\n" +
@@ -74,7 +74,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "    },\n" +
         "    \"r\": {\n" +
         "      \"description\": \"Recovery Group\",\n" +
-        "      \"type\": \"array\",\n" +
+        "      \"type\": [\"null\", \"array\"],\n" +
         "      \"items\": {\n" +
         "        \"\$ref\": \"#/\$defs/recovery_entry\"\n" +
         "      },\n" +
@@ -84,17 +84,17 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "  \"\$defs\": {\n" +
         "    \"dose_posint\": {\n" +
         "      \"description\": \"Dose Number / Total doses in Series: positive integer, range: [1,9]\",\n" +
-        "      \"type\": \"integer\",\n" +
+        "      \"type\": [\"null\", \"integer\"],\n" +
         "      \"minimum\": 1,\n" +
         "      \"maximum\": 9\n" +
         "    },\n" +
         "    \"country_vt\": {\n" +
         "      \"description\": \"Country of Vaccination / Test, ISO 3166 where possible\",\n" +
-        "      \"type\": \"string\"\n" +
+        "      \"type\": [\"null\", \"string\"]\n" +
         "    },\n" +
         "    \"issuer\": {\n" +
         "      \"description\": \"Certificate Issuer\",\n" +
-        "      \"type\": \"string\",\n" +
+        "      \"type\": [\"null\", \"string\"],\n" +
         "      \"maxLength\": 50\n" +
         "    },\n" +
         "    \"person_name\": {\n" +
@@ -102,12 +102,12 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "      \"required\": [\n" +
         "        \"fnt\"\n" +
         "      ],\n" +
-        "      \"type\": \"object\",\n" +
+        "      \"type\": [\"null\", \"object\"],\n" +
         "      \"properties\": {\n" +
         "        \"fn\": {\n" +
         "          \"title\": \"Family name\",\n" +
         "          \"description\": \"The family or primary name(s) of the person addressed in the certificate\",\n" +
-        "          \"type\": \"string\",\n" +
+        "          \"type\": [\"null\", \"string\"],\n" +
         "          \"maxLength\": 50,\n" +
         "          \"examples\": [\n" +
         "            \"d'Červenková Panklová\"\n" +
@@ -125,7 +125,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "        \"gn\": {\n" +
         "          \"title\": \"Given name\",\n" +
         "          \"description\": \"The given name(s) of the person addressed in the certificate\",\n" +
-        "          \"type\": \"string\",\n" +
+        "          \"type\": [\"null\", \"string\"],\n" +
         "          \"maxLength\": 50,\n" +
         "          \"examples\": [\n" +
         "            \"Jiřina-Maria Alena\"\n" +
@@ -134,7 +134,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "        \"gnt\": {\n" +
         "          \"title\": \"Standardised given name\",\n" +
         "          \"description\": \"The given name(s) of the person transliterated\",\n" +
-        "          \"type\": \"string\",\n" +
+        "          \"type\": [\"null\", \"string\"],\n" +
         "          \"maxLength\": 50,\n" +
         "          \"examples\": [\n" +
         "            \"JIRINA<MARIA<ALENA\"\n" +
@@ -144,7 +144,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "    },\n" +
         "    \"certificate_id\": {\n" +
         "      \"description\": \"Certificate Identifier, format as per UVCI: Annex 2 in  https://ec.europa.eu/health/sites/health/files/ehealth/docs/vaccination-proof_interoperability-guidelines_en.pdf\",\n" +
-        "      \"type\": \"string\",\n" +
+        "      \"type\": [\"null\", \"string\"],\n" +
         "      \"maxLength\": 50\n" +
         "    },\n" +
         "    \"vaccination_entry\": {\n" +
@@ -161,7 +161,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "        \"is\",\n" +
         "        \"ci\"\n" +
         "      ],\n" +
-        "      \"type\": \"object\",\n" +
+        "      \"type\": [\"null\", \"object\"],\n" +
         "      \"properties\": {\n" +
         "        \"tg\": {\n" +
         "          \"description\": \"disease or agent targeted\",\n" +
@@ -219,7 +219,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "        \"is\",\n" +
         "        \"ci\"\n" +
         "      ],\n" +
-        "      \"type\": \"object\",\n" +
+        "      \"type\": [\"null\", \"object\"],\n" +
         "      \"properties\": {\n" +
         "        \"tg\": {\n" +
         "          \"\$ref\": \"#/\$defs/disease-agent-targeted\"\n" +
@@ -230,7 +230,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "        },\n" +
         "        \"nm\": {\n" +
         "          \"description\": \"NAA Test Name\",\n" +
-        "          \"type\": \"string\"\n" +
+        "          \"type\": [\"null\", \"string\"]\n" +
         "        },\n" +
         "        \"ma\": {\n" +
         "          \"description\": \"RAT Test name and manufacturer\",\n" +
@@ -243,7 +243,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "        },\n" +
         "        \"dr\": {\n" +
         "          \"description\": \"Date/Time of Test Result\",\n" +
-        "          \"type\": \"string\",\n" +
+        "          \"type\": [\"null\", \"string\"],\n" +
         "          \"format\": \"date-time\"\n" +
         "        },\n" +
         "        \"tr\": {\n" +
@@ -252,7 +252,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "        },\n" +
         "        \"tc\": {\n" +
         "          \"description\": \"Testing Centre\",\n" +
-        "          \"type\": \"string\",\n" +
+        "          \"type\": [\"null\", \"string\"],\n" +
         "          \"maxLength\": 50\n" +
         "        },\n" +
         "        \"co\": {\n" +
@@ -280,7 +280,7 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "        \"du\",\n" +
         "        \"ci\"\n" +
         "      ],\n" +
-        "      \"type\": \"object\",\n" +
+        "      \"type\": [\"null\", \"object\"],\n" +
         "      \"properties\": {\n" +
         "        \"tg\": {\n" +
         "          \"\$ref\": \"#/\$defs/disease-agent-targeted\"\n" +
@@ -316,32 +316,32 @@ const val JSON_SCHEMA_V1 = "{\n" +
         "    },\n" +
         "    \"disease-agent-targeted\": {\n" +
         "      \"description\": \"EU eHealthNetwork: Value Sets for Digital Green Certificates. version 1.0, 2021-04-16, section 2.1\",\n" +
-        "      \"type\": \"string\",\n" +
+        "      \"type\": [\"null\", \"string\"],\n" +
         "      \"valueset-uri\": \"valuesets/disease-agent-targeted.json\"\n" +
         "    },\n" +
         "    \"vaccine-prophylaxis\": {\n" +
         "      \"description\": \"EU eHealthNetwork: Value Sets for Digital Green Certificates. version 1.0, 2021-04-16, section 2.2\",\n" +
-        "      \"type\": \"string\",\n" +
+        "      \"type\": [\"null\", \"string\"],\n" +
         "      \"valueset-uri\": \"valuesets/vaccine-prophylaxis.json\"\n" +
         "    },\n" +
         "    \"vaccine-medicinal-product\": {\n" +
         "      \"description\": \"EU eHealthNetwork: Value Sets for Digital Green Certificates. version 1.0, 2021-04-16, section 2.3\",\n" +
-        "      \"type\": \"string\",\n" +
+        "      \"type\": [\"null\", \"string\"],\n" +
         "      \"valueset-uri\": \"valuesets/vaccine-medicinal-product.json\"\n" +
         "    },\n" +
         "    \"vaccine-mah-manf\": {\n" +
         "      \"description\": \"EU eHealthNetwork: Value Sets for Digital Green Certificates. version 1.0, 2021-04-16, section 2.4\",\n" +
-        "      \"type\": \"string\",\n" +
+        "      \"type\": [\"null\", \"string\"],\n" +
         "      \"valueset-uri\": \"valuesets/vaccine-mah-manf.json\"\n" +
         "    },\n" +
         "    \"test-manf\": {\n" +
         "      \"description\": \"EU eHealthNetwork: Value Sets for Digital Green Certificates. version 1.0, 2021-04-16, section 2.8\",\n" +
-        "      \"type\": \"string\",\n" +
+        "      \"type\": [\"null\", \"string\"],\n" +
         "      \"valueset-uri\": \"valuesets/test-manf.json\"\n" +
         "    },\n" +
         "    \"test-result\": {\n" +
         "      \"description\": \"EU eHealthNetwork: Value Sets for Digital Green Certificates. version 1.0, 2021-04-16, section 2.9\",\n" +
-        "      \"type\": \"string\",\n" +
+        "      \"type\": [\"null\", \"string\"],\n" +
         "      \"valueset-uri\": \"valuesets/test-results.json\"\n" +
         "    }\n" +
         "  }\n" +
